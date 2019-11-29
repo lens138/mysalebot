828e7b0cedce2a86d0fed7adf3127b1cee11ddc6
lens138 <lensgio@gmail.com>
2019-11-29 10:27:35 +0200

    test n1

--- src/main/java/test/MySaleBot.java
+++ src/main/java/test/MySaleBot.java
@@ -1,48 +1,95 @@
 package test;
 
-
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MySaleBot extends TelegramLongPollingBot {
+
     public static void main(String[] args) {
-        ApiContextInitializer.init(); // Инициализируем апи
-        TelegramBotsApi botapi = new TelegramBotsApi();
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
         try {
-            botapi.registerBot(new MySaleBot());
+            telegramBotsApi.registerBot(new MySaleBot());
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
     }
-    @Override
-    public String getBotUsername() {
-        return "MySaleBot";
-        //возвращаем юзера
-    }
 
     @Override
-    public void onUpdateReceived(Update e) {
-        Message msg = e.getMessage(); // Это нам понадобится
-        String txt = msg.getText();
-        if (txt.equals("/start")) {
-            sendMsg(msg, "Hello, world! This is simple bot!");
-        }
+    public String getBotUsername() {
+        return "javasaletestbot";
     }
 
     @Override
     public String getBotToken() {
-        return "1011415059:AAG2MRbSUpAf_f9fyMm_v9p_aiACtJ7qMoQ";
+        return "1052673395:AAFxR2GnLS1LqUIO3p794RbvZ3KoJmz3NYc";
     }
-    @SuppressWarnings("deprecation") // Означает то, что в новых версиях метод уберут или заменят
-    private void sendMsg(Message msg, String text) {
-        SendMessage s = new SendMessage();
-        s.setChatId(msg.getChatId()); // Боту может писать не один человек, и поэтому чтобы отправить сообщение, грубо говоря нужно узнать куда его отправлять
-        s.setText(text);
 
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+
+            if (message.getText().equals("/help"))
+                sendMsg(message, "красава");
+            else
+                sendMsg(message, "none");
+        }
     }
+
+/*private void usrNm(User username, String text) {
+
+}*/
+    private void sendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        // Создаем клавиуатуру
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        // Создаем список строк клавиатуры
+        List<KeyboardRow> keyboard = new ArrayList<>();
+
+        // Первая строчка клавиатуры
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        // Добавляем кнопки в первую строчку клавиатуры
+        keyboardFirstRow.add("/help");
+        keyboardFirstRow.add("none");
+
+        // Вторая строчка клавиатуры
+        //KeyboardRow keyboardSecondRow = new KeyboardRow();
+        // Добавляем кнопки во вторую строчку клавиатуры
+       //keyboardSecondRow.add("Комманда 3");
+        //keyboardSecondRow.add("Комманда 4");
+
+        //Добавляем все строчки клавиатуры в список
+        keyboard.add(keyboardFirstRow);
+       // keyboard.add(keyboardSecondRow);
+        // и устанваливаем этот список нашей клавиатуре
+        replyKeyboardMarkup.setKeyboard(keyboard);
+        try {
+            execute(sendMessage);
+        }
+        catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
